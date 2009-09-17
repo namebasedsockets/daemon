f521a3c6583c6a00957f7050acbf6e7ef30655fc
Juan Lang <juan.lang@ericsson.com>
2009-09-17 15:33:51 -0700

    Add dns.c to android build

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := namestackd
-LOCAL_SRC_FILES := daemon.c NameStackDaemon.cpp
+LOCAL_SRC_FILES := daemon.c dns.c NameStackDaemon.cpp
 LOCAL_CPP_FLAGS := -DANDROID
 
 include $(BUILD_SHARED_LIBRARY)
